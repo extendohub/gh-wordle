b77220ad2860d5c0d57163b8a553d257b4fb8823
Jeff McAffer <jeffmcaffer@github.com>
2022-02-04 20:04:56 +0000

    remove debugging

--- .github/events/http/wordle/_guess(...).js
+++ .github/events/http/wordle/_guess(...).js
@@ -96,8 +96,6 @@ async function pickNewWord() {
 
 async function getWords() {
   const response = await helpers.octokit.repos.getContent({ owner: 'extendohub', repo: 'gh-wordle', path: 'words.json' })
-  console.log(`And the words content is `)
-  console.dir(response)
   if (response.statusCode < 200 || response.statusCode >= 300) throw new Error('There was a problem loading words!')
   return JSON.parse(Buffer.from(response.data.content, 'base64').toString('utf8'))
 }
